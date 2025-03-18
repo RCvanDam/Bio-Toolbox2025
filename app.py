0d77a386522dcaf92d3e3dc3f6b143e3d1df6cb9
Emiel <emiellb03@hotmail.nl>
2025-03-18 10:36:27 +0100

    This added the /kegg-pull path to app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/kegg_pull')
+def kegg_pull():  # put application's code here
+    return render_template('tool.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
