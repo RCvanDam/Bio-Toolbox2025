bb06505f0405b61e53346c0a7bf5595e071151c0
RCvanDam <144681411+RCvanDam@users.noreply.github.com>
2025-04-01 11:26:08 +0200

    Update backend.py

--- backend.py
+++ backend.py
@@ -60,7 +60,7 @@ class KEGGHandler:
             may be needed for different pathway types or irregular data formats.
         """
         nodes, edges = [], []
-        # print(kegg_data)
+        print(kegg_data)
         # Extract genes and compounds as nodes
         for line in kegg_data.split("\n"):
             if line.startswith("GENE"):
